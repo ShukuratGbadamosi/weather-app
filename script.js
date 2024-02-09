3f6f0c20f155bb825f870186497872ca9769b922
gbadamosi shukurat opeyemi <gbadamosishukurat2006@gmail.com>
2024-02-09 15:39:42 +0100

    tenth commit

--- script.js
+++ script.js
@@ -52,23 +52,23 @@ const getLocation = () => {
     }
 }
 //     function getWeatherByCoordinates(latitude, longitude) {
-//         const apikey = "62a88c5960ba9663ad4477e1bd5d894b"
-//         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`)
-//         .then(response => response.json())
-//         .then(data => {
-//             const temperature = data.main.temp;
-//             const humidity = data.main.humidity;
-//             const description = data.weather[0].description;
-//             const windSpeed = data.wind.speed;
+// //         const apikey = "62a88c5960ba9663ad4477e1bd5d894b"
+// //         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=metric`)
+// //         .then(response => response.json())
+// //         .then(data => {
+// //             const temperature = data.main.temp;
+// //             const humidity = data.main.humidity;
+// //             const description = data.weather[0].description;
+// //             const windSpeed = data.wind.speed;
 
-//         temp.innerHTML = `${(data.main.temp).toFixed(1)}°C`;
-//         desc.innerHTML = `${(data.weather[0].description)}`;
-//         humid.innerHTML = `${(data.main.humidity)}%`;
-//         windspeed.innerHTML = `${(data.wind.speed)}m/s`;
-//         })
-//         .catch(error => {
-//             console.error("Error feching data error", error);
+// //         temp.innerHTML = `${(data.main.temp).toFixed(1)}°C`;
+// //         desc.innerHTML = `${(data.weather[0].description)}`;
+// //         humid.innerHTML = `${(data.main.humidity)}%`;
+// //         windspeed.innerHTML = `${(data.wind.speed)}m/s`;
+// //         })
+// //         .catch(error => {
+// //             console.error("Error feching data error", error);
         
-//         });
-//     }
+// //         });
+//    }
     
